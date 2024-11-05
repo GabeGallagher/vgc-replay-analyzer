eb9b1b014b0f494a91b996d9933b5dd34f744078
Gabriel B. Gallagher <zeinfandel@yahoo.com>
2024-11-05 15:33:05 -0600

    added best and worst matchups

--- src/components/matchupStats/matchupStats.tsx
+++ src/components/matchupStats/matchupStats.tsx
@@ -34,11 +34,73 @@ const MatchupStats = ({ replayEntries, usageMap, spriteMap }) => {
     loadMatchupStats();
   }, [replayEntries]);
 
+  /* Builds a list of up to 6 pokemon against whom player has the most wins. List is sorted from
+  most wins against to least wins, as long as the pokemon was seen at least 3 times. List not
+  intended to be sorted by win percentage. All these design decisions may be changed later, but are
+  intended behavior, not bugs*/
+  const buildBestMatchups: JSX.Element[] = useMemo(() => {
+    const elements: JSX.Element[] = [];
+    const sortedEntries: [string, [number, number]][] = Array.from(
+      opponentPokemonMap.entries()
+    ).sort((a, b) => b[1][1] - a[1][1]); // Sort from highest to lowest games won
+
+    let opponentPokemonArray: [string, [number, number]][] = [];
+
+    if (sortedEntries.length >= 6) {
+      for (let i = 0; i < 6; i++) opponentPokemonArray.push(sortedEntries[i]);
+    } else {
+      opponentPokemonArray = sortedEntries;
+    }
+
+    for (let i = 0; i < opponentPokemonArray.length; i++) {
+      const winCount: number = opponentPokemonArray[i][1][0] - opponentPokemonArray[i][1][1];
+      elements.push(
+        <div key={opponentPokemonArray[i][0]} className="pokemon-matchup-container">
+          <img src={getSpritePath(opponentPokemonArray[i][0])} alt={opponentPokemonArray[i][0]} />
+          <p>
+            Won {winCount} of {opponentPokemonArray[i][1][0]} games
+            {((winCount / opponentPokemonArray[i][1][0]) * 100).toFixed(2)}%
+          </p>
+        </div>
+      );
+    }
+    return elements;
+  }, [opponentPokemonMap, replayEntries]);
+
+  const buildWorstMatchups: JSX.Element[] = useMemo(() => {
+    const elements: JSX.Element[] = [];
+    const sortedEntries: [string, [number, number]][] = Array.from(
+      opponentPokemonMap.entries()
+    ).sort((a, b) => a[1][1] - b[1][1]); // Sort from lowest to highest games won
+
+    let opponentPokemonArray: [string, [number, number]][] = [];
+
+    if (sortedEntries.length >= 6) {
+      for (let i = 0; i < 6; i++) opponentPokemonArray.push(sortedEntries[i]);
+    } else {
+      opponentPokemonArray = sortedEntries;
+    }
+
+    for (let i = 0; i < opponentPokemonArray.length; i++) {
+      const lossCount: number = opponentPokemonArray[i][1][1];
+      elements.push(
+        <div key={opponentPokemonArray[i][0]} className="pokemon-matchup-container">
+          <img src={getSpritePath(opponentPokemonArray[i][0])} alt={opponentPokemonArray[i][0]} />
+          <p>
+            Lost {lossCount} of {opponentPokemonArray[i][1][0]} games
+            {((lossCount / opponentPokemonArray[i][1][0]) * 100).toFixed(2)}%
+          </p>
+        </div>
+      );
+    }
+    return elements;
+  }, [opponentPokemonMap, replayEntries]);
+
   const buildHighestAttendence: JSX.Element[] = useMemo(() => {
     const elements: JSX.Element[] = [];
     const sortedEntries: [string, [number, number]][] = Array.from(
       opponentPokemonMap.entries()
-    ).sort((a, b) => b[1][0] - a[1][0]); // Sort from highest to lowest
+    ).sort((a, b) => b[1][0] - a[1][0]); // Sort from highest to lowest games appeared
 
     let opponentPokemonArray: [string, [number, number]][] = [];
 
@@ -67,7 +129,7 @@ const MatchupStats = ({ replayEntries, usageMap, spriteMap }) => {
     const elements: JSX.Element[] = [];
     const sortedEntries: [string, [number, number]][] = Array.from(
       opponentPokemonMap.entries()
-    ).sort((a, b) => a[1][0] - b[1][0]); // Sort from lowest to highest
+    ).sort((a, b) => a[1][0] - b[1][0]); // Sort from lowest to highest games appeared
 
     let opponentPokemonArray: [string, [number, number]][] = [];
 
@@ -94,8 +156,14 @@ const MatchupStats = ({ replayEntries, usageMap, spriteMap }) => {
 
   return (
     <div className="matchup-stats-container">
-      <div>Best Matchups</div>
-      <div>Worst Matchups</div>
+      <div>
+        <h2>Best Matchups</h2>
+        <div className="matchup-stats">{buildBestMatchups}</div>
+      </div>
+      <div>
+        <h2>Worst Matchups</h2>
+        <div className="matchup-stats">{buildWorstMatchups}</div>
+      </div>
       <div>
         <h2>Highest Attendence</h2>
         <div className="matchup-stats">{buildHighestAttendence}</div>
